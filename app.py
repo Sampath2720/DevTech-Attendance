ab678895debf5da038d1ceabcf48afa71bf88b12
Sampath <sampathmallampalli27@gmail.com>
2026-09-11 06:18:22 +0000

    Version 5.0 UI Portal

--- app.py
+++ app.py
@@ -6,11 +6,40 @@ app = Flask(__name__)
 def home():
     return """
     <html>
-    <body style="font-family:Arial;text-align:center">
-    <h1>DevTech Attendance Portal</h1>
-    <h3>Version 4.0</h3>
-    <p>CI Pipeline Successfully Implemented</p>
-    <p>Hosted on Azure VM using Docker</p>
+    <head>
+        <title>DevTech Attendance Portal</title>
+    </head>
+    <body style="font-family:Arial;padding:30px">
+
+        <h1>DevTech Attendance Portal</h1>
+        <h3>Version 5.0</h3>
+
+        <form>
+            <label>Employee ID</label><br>
+            <input type="text"><br><br>
+
+            <label>Employee Name</label><br>
+            <input type="text"><br><br>
+
+            <label>Domain</label><br>
+            <select>
+                <option>Linux Admin</option>
+                <option>Windows Admin</option>
+                <option>Cloud Engineer</option>
+                <option>DevOps Engineer</option>
+            </select><br><br>
+
+            <label>Date</label><br>
+            <input type="date"><br><br>
+
+            <label>Status</label><br>
+            <input type="radio" name="status"> Present
+            <input type="radio" name="status"> Absent
+            <br><br>
+
+            <button type="submit">Submit Attendance</button>
+        </form>
+
     </body>
     </html>
     """
